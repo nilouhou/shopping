6a209d01ff863819afa0aa9763350b676ef8bf17
Nilou <nilou.houshmand@gmail.com>
2022-02-03 16:15:06 -0800

    initial folder

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+	height: 60px;
+	background-color: black;
+`;
+
+const Header = () => {
+	return (
+		<Container>
+			<h1>h1</h1>
+		</Container>
+	);
+};
+
+export default Header;
