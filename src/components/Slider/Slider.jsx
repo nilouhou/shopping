311a26296b66fa335500817aaa807e63ebccf741
Nilou <nilou.houshmand@gmail.com>
2022-03-11 12:50:02 -0800

    :creating slider arrow

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import ArrowLeftOutlinedIcon from "@mui/icons-material/ArrowLeftOutlined";
+import styled from "styled-components";
 
+const Container = styled.div`
+	width: 100%;
+	height: 100vh;
+	display: flex;
+`;
+
+const Arrow = styled.div`
+	with: 50px;
+	height: 50px;
+	background-color: #fff7f7;
+	border-radius: 50%;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+`;
 const Slider = () => {
-	return <div>slide</div>;
+	return;
+	<Container>
+		<Arrow>
+			<ArrowLeftOutlinedIcon />
+		</Arrow>
+	</Container>;
 };
 
 export default Slider;
