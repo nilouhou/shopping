5724d0e4b114b10cc3f3d973a70d131a44317d2d
Nilou <nilou.houshmand@gmail.com>
2022-03-11 13:09:33 -0800

    added direction as props and style based on that

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -8,6 +8,7 @@ const Container = styled.div`
 	width: 100%;
 	height: 100vh;
 	display: flex;
+	position: relative;
 `;
 
 const Arrow = styled.div`
@@ -18,15 +19,21 @@ const Arrow = styled.div`
 	display: flex;
 	align-items: center;
 	justify-content: center;
+	position: absolute;
+	top: 0;
+	bottom: 0;
+	margin: auto;
+	left: ${(props) => props.direction === "left" && "10px"};
+	right: ${(props) => props.direction === "right" && "10px"};
 `;
 const Slider = () => {
 	return (
 		<Container>
-			<Arrow>
+			<Arrow direction="left">
 				<ArrowLeftOutlinedIcon />
 			</Arrow>
 
-			<Arrow>
+			<Arrow direction="right">
 				<ArrowRightOutlinedIcon />
 			</Arrow>
 		</Container>
