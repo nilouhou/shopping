dc3e9cd88c0c8004d236b8b0d44395a0eb45dc9e
Nilou <nilou.houshmand@gmail.com>
2022-03-14 09:21:21 -0700

    style the category menu items

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import { categories } from "../../data/data";
 import CategoryItem from "../CategoryItem/CategoryItem";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+	display: flex;
+	padding: 8px;
+	gap: 8px;
+`;
 
 const Categories = () => {
 	return (
-		<div>
+		<Wrapper>
 			{categories.map((category) => (
 				<CategoryItem key={category.id} category={category} />
 			))}
-		</div>
+		</Wrapper>
 	);
 };
 
