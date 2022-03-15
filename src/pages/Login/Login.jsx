b50d4b0c09df6597da9544ca98dee0b5e34a3974
Nilou <nilou.houshmand@gmail.com>
2022-03-15 15:36:45 -0700

    added authentication for google popup and use it in comonent

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,5 +1,15 @@
+import { signInWithGooglePopup } from "../../utils/firbase/firebase";
 const Login = () => {
-	return <div>Login</div>;
+	const logGoogleUser = async () => {
+		const response = await signInWithGooglePopup();
+		console.log(response);
+	};
+	return (
+		<div>
+			<h1>Login page</h1>
+			<button onClick={logGoogleUser}>Sign in with Google</button>
+		</div>
+	);
 };
 
 export default Login;
--- src/utils/firbase/firebase.js
+++ src/utils/firbase/firebase.js
@@ -1,14 +1,29 @@
 import { initializeApp } from "firebase/app";
+import {
+	GoogleAuthProvider,
+	getAuth,
+	signInWithPopup,
+	signInWithRedirect,
+} from "firebase/auth";
 
 //web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: "AIzaSyDX8o4WlMYwIG7uECQyII5u8s0FfsQgfG0",
-	authDomain: "baritzia-943f6.firebaseapp.com",
-	projectId: "baritzia-943f6",
-	storageBucket: "baritzia-943f6.appspot.com",
-	messagingSenderId: "1030972775586",
-	appId: "1:1030972775586:web:9e285126daf0c13db80bfb",
+	apiKey: process.env.REACT_APP_apiKey,
+	authDomain: process.env.REACT_APP_authDomain,
+	projectId: process.env.REACT_APP_projectId,
+	storageBucket: process.env.REACT_APP_storageBucket,
+	messagingSenderId: process.env.REACT_APP_messagingSenderId,
+	appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider(); // it is a class and we can have many providers in app that is why we are making instance of it
+
+provider.setCustomParameters({
+	propmt: "select_account",
+});
+
+export const auth = getAuth(); // we only need one rule of authentication, so we don't need instance and new
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
