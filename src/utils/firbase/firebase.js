a66f231692dc19f3a15299b4a69305ab0658a240
Nilou <nilou.houshmand@gmail.com>
2022-03-16 14:20:02 -0700

    setup the confirguration for firestore

--- src/utils/firbase/firebase.js
+++ src/utils/firbase/firebase.js
@@ -1,10 +1,18 @@
+//import firebase app
 import { initializeApp } from "firebase/app";
+
+//import firebase authentication methods
 import {
 	GoogleAuthProvider,
 	getAuth,
 	signInWithPopup,
 	signInWithRedirect,
 } from "firebase/auth";
+
+//import firebase storedata
+//doc = documentation : shoes is collection, nike max is document
+//getDoc= getting data of document, for example get data of nike max price, size
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
 //web app's Firebase configuration
 const firebaseConfig = {
@@ -19,6 +27,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 
+// creating Authentication provider
 const provider = new GoogleAuthProvider(); // it is a class and we can have many providers in app that is why we are making instance of it
 
 provider.setCustomParameters({
@@ -27,3 +36,14 @@ provider.setCustomParameters({
 
 export const auth = getAuth(); // we only need one rule of authentication, so we don't need instance and new
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+//Create database instance
+export const db = getFirestore();
+
+//how to use the database, create a method to use it
+export const createUserDocrumentFromAuth = async (userAuth) => {
+	//doc is getting 3 argument, db, name of collection in database and unique id
+	const userDocRef = doc(db, "user", userAuth.uid);
+
+	const userData = await getDoc(userDocRef);
+};
