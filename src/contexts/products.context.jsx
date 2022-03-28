530efa506b3faf1460d3a62027c490a4f6fd3d7c
Nilou <nilou.houshmand@gmail.com>
2022-03-28 10:38:54 -0700

    fetching data from firestore and display it in frontend

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -3,21 +3,21 @@ import SHOP_DATA from "../data/shop-data";
 import { getDataFromFireStore } from "../utils/firbase/firebase";
 
 export const ProductContext = createContext({
-	products: null,
+	products: {},
 });
 
 export const ProductProvier = ({ children }) => {
-	const [products, setProducts] = useState([]);
+	const [products, setProducts] = useState({});
 	const value = { products };
 
 	useEffect(() => {
 		const getData = async () => {
 			const catgoriesMap = await getDataFromFireStore();
-			console.log(catgoriesMap);
+			setProducts(catgoriesMap);
 		};
-
 		getData();
 	}, []);
+
 	return (
 		<ProductContext.Provider value={value}>{children}</ProductContext.Provider>
 	);
--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -5,11 +5,21 @@ import "./Shop.scss";
 
 export const Shop = () => {
 	const { products } = useContext(ProductContext);
+	console.log(products);
 	return (
-		<div className="products-container">
-			{products.map((product) => (
-				<ProductItem key={product.id} product={product} />
-			))}
+		<div className="main-container">
+			{Object.keys(products).map((categoriesTitle) => {
+				return (
+					<div key={categoriesTitle}>
+						<h1 className="title">{categoriesTitle}</h1>
+						<div className="products-container">
+							{products[categoriesTitle].map((product) => (
+								<ProductItem key={product.id} product={product} />
+							))}
+						</div>
+					</div>
+				);
+			})}
 		</div>
 	);
 };
