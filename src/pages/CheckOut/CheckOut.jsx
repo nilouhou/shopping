4b1ac81e10bd9842f86dad1ed1ad4182a4ad2c13
Nilou <nilou.houshmand@gmail.com>
2022-03-31 11:22:13 -0700

    convert cehcout sass file to styled component

--- src/pages/CheckOut/CheckOut.jsx
+++ src/pages/CheckOut/CheckOut.jsx
@@ -1,37 +1,42 @@
 import React, { useContext } from "react";
 import CheckOutItem from "../../components/CheckOutItem/CheckOutItem";
 import { CartContext } from "../../contexts/cart.context";
-import "./CheckOut.scss";
+import {
+	CheckoutContainer,
+	CheckoutHeader,
+	Total,
+	HeaderBlock,
+} from "./CheckOut.style";
 
 const CheckOut = () => {
 	const { cartItems, total } = useContext(CartContext);
 	return (
-		<div className="checkout-container">
+		<CheckoutContainer>
 			<h1>Checkout</h1>
-			<div className="checkout-header">
-				<div className="header-block">
+			<CheckoutHeader>
+				<HeaderBlock>
 					<span>Product</span>
-				</div>
-				<div className="header-block">
+				</HeaderBlock>
+				<HeaderBlock>
 					<span>Description</span>
-				</div>
-				<div className="header-block">
+				</HeaderBlock>
+				<HeaderBlock>
 					<span>Quantity</span>
-				</div>
-				<div className="header-block">
+				</HeaderBlock>
+				<HeaderBlock>
 					<span>Price</span>
-				</div>
-				<div className="header-block">
+				</HeaderBlock>
+				<HeaderBlock>
 					<span>Remove</span>
-				</div>
-			</div>
+				</HeaderBlock>
+			</CheckoutHeader>
 			{cartItems &&
 				cartItems.map((cartItem) => (
 					<CheckOutItem key={cartItem.id} cartItem={cartItem} />
 				))}
 
-			<div className="total">Total:${total} </div>
-		</div>
+			<Total>Total:${total} </Total>
+		</CheckoutContainer>
 	);
 };
 
